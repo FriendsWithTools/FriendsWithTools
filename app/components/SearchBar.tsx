a2a83940f93c39f906c37b2a3ecc49944742e567
Brice <fromm.brice@gmail.com>
2024-06-25 08:19:51 +0200

    updated CSS of searchbar

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -5,15 +5,15 @@ import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 const SearchBar = () => {
   return (
     <div className='flex justify-center pt-6 pb-6'>
-      <div className='relative flex items-center justify-center w-11/12 rounded-full '>
-        <MagnifyingGlassIcon className='absolute left-2 top-1/2 h-4 w-4 -translate-y-1/2 transform' />
+      <form className='relative flex items-center justify-center w-11/12  '>
+        <MagnifyingGlassIcon className='absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 transform stroke-2' />
         <Input
           placeholder='What do you wish to rent?'
           // value={search}
           // onChange={(event) => setSearch(event.target.value)}
-          className=' pl-8 focus-visible:ring-offset-0 focus-visible:ring-0 rounded-3xl h-12'
+          className='shadow-md pl-10 focus-visible:ring-offset-0 focus-visible:ring-0 rounded-3xl h-12'
         />
-      </div>
+      </form>
     </div>
   );
 };
